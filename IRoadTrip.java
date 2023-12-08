643ba12667cb8b0b606a20288e231699dc5eb5f2
stevenbuks <114451484+stevenbuks@users.noreply.github.com>
2023-12-07 21:51:16 -0800

    Project 03 Submission

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -13,6 +13,53 @@ public class IRoadTrip
     String borders;
     String capDist;
     String stateName;
+    //this is a class that I use when I add a path of countries  and their distances into a priority queue
+    //this is so we can compare one entry into the priority queue into another because each entry is a hashmap so there is nothing to compare it to by default
+    class HeapElement implements Comparable<HeapElement>
+    {
+        //this is the string that holds a path of countries (e.g. Mexico/United States/Canada)
+        String country;
+        //this holds the distance of the whole string path
+        Integer dist;
+        //constructor that sets the values of the class level varaibles to be the values given by the user when a new heap entry is made
+        HeapElement(String countryPath, Integer distance)
+        {
+            country = countryPath;
+            dist = distance;
+        }
+        //this is the compareTo method that allows us to compare one heap entry to another
+        //essentially if the distance of the path passed in is less than other distance -> return -1
+        //if they are equal return 0
+        //else return 1
+        @Override
+        public int compareTo(HeapElement o)
+        {
+            if (this.dist < o.dist)
+            {
+                return -1;
+            }
+            else if (dist == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        //this getter method returns the distance of the current object
+        Integer getDist()
+        {
+            return this.dist;
+        }
+        //this getter method returns the path of the current object
+        String getPath()
+        {
+            return this.country;
+        }
+    }
+    //this is the subclass where our map is created
+    // it created by reading multiple files with information regarding countries and the distance between other countries
     class Graph
     {
         //this map converts from full name of country to its acronym
@@ -30,7 +77,9 @@ public class IRoadTrip
         {
             try
             {
+                //name ID map converts from acronym to name
                 nameIDMap = new HashMap<>();
+                //this made converts from name to acronym
                 nameIDMapReverse = new HashMap<>();
                 //scanner is used to read file
                 Scanner scan = new Scanner(new File(fileName));
@@ -49,7 +98,6 @@ public class IRoadTrip
                         nameIDMapReverse.put(edgeCase(nameLine[1].trim()), edgeCase(nameLine[2]));
                     }
                 }
-                System.out.println(nameIDMap);
             }
             catch (FileNotFoundException fe)
             {
@@ -59,7 +107,6 @@ public class IRoadTrip
         //reads borders.txt
         public void readBordersFile(String fileName)
         {
-            //Countries = new HashMap<>();
             try
             {
                 //scanner used to read file
@@ -145,7 +192,6 @@ public class IRoadTrip
                         }
                     }
                 }
-                System.out.println(CountryMap);
             }
             catch(FileNotFoundException fe)
             {
@@ -201,7 +247,6 @@ public class IRoadTrip
                         }
                     }
                 }
-                System.out.println(CountryMap);
             }
             catch (FileNotFoundException fe)
             {
@@ -303,6 +348,10 @@ public class IRoadTrip
             {
                 return "Korea, North";
             }
+            else if (CountryName.equals("England"))
+            {
+                return "United Kingdom";
+            }
             else if (CountryName.equals("South Korea"))
             {
                 return "Korea, South";
@@ -367,6 +416,14 @@ public class IRoadTrip
             {
                 return "Lithuania";
             }
+            else if (CountryName.equals("Denmark (Greenland)"))
+            {
+                return "Denmark";
+            }
+            else if (CountryName.equals("US"))
+            {
+                return "United States";
+            }
             return CountryName;
         }
     }
@@ -374,7 +431,7 @@ public class IRoadTrip
     public IRoadTrip (String [] args)
     {
         //the constructor makes the graph object read the files
-
+        //additionally it retrieves the command line arguments and associates them with files
         try
         {
             //this reads in the command line arguments
@@ -392,10 +449,9 @@ public class IRoadTrip
         }
 
     }
-//getDistance returns the distnace between two adjacent countries
+//getDistance returns the distance between two adjacent countries
     public int getDistance (String country1, String country2)
     {
-        HashMap<String, Integer> borderMap = new HashMap<>();
         try
         {
             return g.CountryMap.get(country1).get(country2);
@@ -408,96 +464,83 @@ public class IRoadTrip
     //this is the method where we make a path between two countries
     public List<String> findPath (String country1, String country2)
     {
-        // Make new heap that contains a string that represents a path of countries where which the countries are separated by a "/"
+        // Make new heap that contains an object that has a string that represents a path of countries where which the countries are separated by a "/"
         //e.g. path from US to Canada would be United States/Canada
-        HashMap <String,Integer> heapMap = new HashMap<String, Integer>();
-        PriorityQueue<HashMap <String, Integer>> heap = new PriorityQueue();
-        heapMap.put(country1, 0);
+        //the object also holds an Integer value that is the distance of that path
+        PriorityQueue<HeapElement> heap = new PriorityQueue<>();
         //add the input country and its distance ie 0 to heap
-        heap.offer(heapMap);
-        //initialize a destination Heap
-        //this heap only contains paths from the origin to the destination
-        //this is so we can get the fastest path
-        HashMap<String, Integer> DestMap = new HashMap<>();
-        PriorityQueue<HashMap<String, Integer>> DestHeap = new PriorityQueue<>();
-        int total;
+        HeapElement He = new HeapElement(country1, Integer.valueOf(0));
+        heap.add(He);
+        Integer total;
         //while the main path heap isn't empty
         while (!heap.isEmpty())
         {
-            //get the path string and its distance
-           HashMap<String, Integer> heapEntry =  heap.poll();
-           //given a hashmap entry in heap we then iterate to get its key, since there is only one key per entry this loop should actually be O(1)
-           for (String origin : heapEntry.keySet())
-           {
-               //then we get the distance given that path string
-               total = heapEntry.get(origin);
-               //then we split the path string by / to isolate each country in that path
-               //this is important so we can get the last country so we can see its neighbors
-               String[] heapKey = origin.split("/");
+            //get the object whose element has the smallest distnace value
+           HeapElement  heapEntry =  heap.poll();
+           //get that objects path and distance
+           String path = heapEntry.getPath();
+           total = heapEntry.getDist();
+            //then we split the path string by / to isolate each country in that path
+            //this is important so we can get the last country so we can see its neighbors
+            String[] heapKey = path.split("/");
                //this gives me the last country in the path
-               String key = heapKey[heapKey.length -1];
-               //then we get the neighbor
-               for (String destination: g.CountryMap.get(key).keySet())
-               {
-                   if(!origin.contains(destination))
-                   {
-                       //then we get the distance given that path string and reset it
-                       total = heapEntry.get(origin);
-                       //then we get the distance between current country and its neighbors
-                       int distance = getDistance(key, destination);
-                       //we then add the distance to the total value
-                       total += distance;
-                       //then we add the name of the neighbors to the path string
-                       String path = origin + "/" + destination;
-                       //add entry to main heap if and only if we don't add the final destination to the heap
-                       if (!path.contains(country2))
-                       {
-                           heapMap.put(path, total);
-                           heap.offer(heapMap);
-                       }
-                       //otherwise we add that country to the heap of destnations
-                       else
-                       {
-                           DestMap.put(origin, total);
-                           DestHeap.offer(DestMap);
-                       }
-                   }
-               }
+            String key = heapKey[heapKey.length -1];
+            //if the path pulled already has the destination then we know we have the lowest priority
+            if(path.contains(country2))
+            {
+                //make the pathstring into an array list where each index is the individual elements of the path
+                List<String> PathToDest = new ArrayList<>();
+                HeapElement BestPathEntry = heapEntry;
+                String BestPath =  BestPathEntry.getPath();
+                //split string based on "/"
+                String[] CountriesInPath = BestPath.split("/");
+                //add it to the array list
+                for (int i = 0; i < CountriesInPath.length; i++)
+                {
+                    //this adds each individual country from the path onto the list
+                    PathToDest.add(CountriesInPath[i]);
+                }
+                //returns list
+                return PathToDest;
+            }
+            else
+            {
+                //if we get bordering countries from the last country in the path
+                //iterate over those
+                if (g.CountryMap.get(key) != null)
+                {
+                    for (String destination: g.CountryMap.get(key).keySet())
+                    {
+                        //reset path and total after each iteration
+                        path = heapEntry.getPath();
+                        total = heapEntry.getDist();
+                        //if the path doesn't contain a border country already looked at in the path string
+                        if(!path.contains(destination))
+                        {
+                            //then we get the distance between current country and its neighbors
+                            int distance = getDistance(key, destination);
+                            //we then add the distance to the total value
+                            total += Integer.valueOf(distance);
+                            //then we add the name of the neighbors to the path string
+                            path = path + "/" + destination;
+                            //add entry to  heap if and only if we don't add the final destination to the heap
+                            HeapElement addEntry =  new HeapElement(path, total);
+                            heap.add(addEntry);
+                        }
+                    }
+                }
+            }
            }
-           //this makes a list of strings to get the route
-           List<String> PathToDest = new LinkedList<>();
            //if the heap is empty them our path never leads to the destination
             //i.e. our countries are on the other side of the world or one of them is an island
-           if (DestHeap.isEmpty())
-           {
-               return PathToDest;
-           }
-           else
-           {
-               //otherwise just get the highest priority item
-               //we don't need to check the other ones as they would all have a higher distance value
-               HashMap<String, Integer> BestPathMap = DestHeap.poll();
-               //we get the key from the heap that we poll
-               for (String PathKey: BestPathMap.keySet())
-               {
-                   //we split the string based on "/" to get each individual country that makes up the route
-                   String[] CountriesInPath = PathKey.split("/");
-                   for (int i = 0; i < CountriesInPath.length; i++)
-                   {
-                       //this adds each individual country from the path onto the list
-                       PathToDest.add(CountriesInPath[i]);
-                   }
-                   //returns list
-                   return PathToDest;
-               }
-           }
+        //if that is the case we will return an empty list
+        List<String> emptyList = new ArrayList<>();
+        return emptyList;
         }
-        return null;
-    }
     //this accepts the user input and calls edge case
     public void acceptUserInput()
     {
-        //this receives the input for the input countires
+        //this receives the input for the input countries
         System.out.println("Enter the name of the first country (type EXIT to quit):");
         //we read the keyboard to get the input
         Scanner scan = new Scanner(System.in);
@@ -505,27 +548,56 @@ public class IRoadTrip
         //gets the proper name for 1st country
         Country1 = g.edgeCase(Country1);
         //if its not in the map then we assume invalid name
-        if (!g.CountryMap.containsKey(Country1))
+        //repeat until country1 is valid or "EXIT" inputted
+        while (!g.CountryMap.containsKey(Country1))
         {
+            if (Country1.equals("EXIT"))
+            {
+                return;
+            }
             System.out.println("Invalid country name. Please enter a valid country name.");
+            Country1 = scan.nextLine();
+            Country1 = g.edgeCase(Country1);
         }
         System.out.println("Enter the name of the first country (type EXIT to quit):");
         String Country2 =  scan.nextLine();
         //the same is for Country2
         Country2 = g.edgeCase(Country2);
-        if (!g.CountryMap.containsKey(Country2))
+        //repeat until "EXIT" or country2 is valid
+        while(!g.CountryMap.containsKey(Country2))
         {
+            if (Country2.equals("EXIT"))
+            {
+                return;
+            }
             System.out.println("Invalid country name. Please enter a valid country name.");
+            Country2 = scan.nextLine();
+            Country2 = g.edgeCase(Country2);
         }
         //if we get EXIT in input we stop the program
-        if (Country2.equals("EXIT") || Country1.equals("EXIT"))
-        {
-            return;
-        }
-        List<String> Pathway = new LinkedList<>();
+        List<String> Pathway = new ArrayList<>();
+        //gets the list of countries needed for the pathway
         Pathway = findPath(Country1, Country2);
-        System.out.println(Pathway);
+        //report result is the method that properly prints the output
+        reportResult(Pathway);
     }
+    //given a list of strings this method will return a message saying that the inputs were islands if the list was empty
+    //if the list isn't empty the method will iterate over the list and get a county and get its distance from the next country in the list and prints those values
+    void reportResult(List<String> Pathway)
+    {
+        if (Pathway.isEmpty())
+        {
+            System.out.println("Either your input or output is an island");
+        }
+        for (int i = 0; i < Pathway.size()-1; i++)
+        {
+            String country1 = Pathway.get(i);
+            String country2 = Pathway.get(i+1);
+            int length  = getDistance(country1, country2);
+            System.out.println(country1 + " --> " + country2 + " ("+ length + " km.)");
+        }
+    }
+    //in main all we do is call I road trip
     public static void main(String[] args)
     {
         IRoadTrip a3 = new IRoadTrip(args);
